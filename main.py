445fd071a7028521e89ba04cb83bfa0f88dc10f6
brendancicchi <bcicchi@gmail.com>
2021-05-19 16:17:54 -0400

    Added click options for leveraging nodetool ring files instead of connecting to clusters

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import cassandra_utils
 import click
+import json
 from tabulate import tabulate
 
 # Starting code derived from https://stackoverflow.com/a/44349292/10156762
-# Have some options that require other optional arguments to be set
+# Have some options that require other optional arguments to be set if they are specified
 class RequiredIf(click.Option):
     def __init__(self, *args, **kwargs):
         self.required_if = kwargs.pop('required_if').split(',')
@@ -30,9 +31,36 @@ class RequiredIf(click.Option):
         return super(RequiredIf, self).handle_parse_result(
             ctx, opts, args)
 
+class MutuallyExclusive(click.Option):
+    def __init__(self, *args, **kwargs):
+        self.mutually_exclusive = kwargs.pop('mutually_exclusive')
+        assert self.mutually_exclusive, "'mutually_exclusive' parameter required"
+        kwargs['help'] = (kwargs.get('help', '') +
+          ' NOTE: This argument cannot be used with %s' %
+          ','.join(self.mutually_exclusive)
+        ).strip()
+        super(MutuallyExclusive, self).__init__(*args, **kwargs)
+    
+    def handle_parse_result(self, ctx, opts, args):
+        we_are_present = self.name in opts
+        other_present = self.mutually_exclusive in opts
+
+        if we_are_present and other_present:
+            raise click.UsageError(
+                "Illegal usage: `%s` and `%s` are mutually exclusive options" % (
+                        self.name, self.mutually_exclusive))
+        else:
+            self.prompt = None
+        return super(MutuallyExclusive, self).handle_parse_result(
+            ctx, opts, args)
+            
+
 @click.command()
+# Cluster Ring Output
+@click.option('--source-ring-file', '-sf', help='Nodetool ring output for source cluster', cls=MutuallyExclusive, mutually_exclusive='source_hosts')
+@click.option('--target-ring-file', '-tf', help='Nodetool ring output for target cluster', cls=MutuallyExclusive, mutually_exclusive='target_hosts')
 # Source cluster connection options
-@click.option('--source-hosts', '-sh', default='127.0.0.1', required=True, help='Initial contact points for source cluster')
+@click.option('--source-hosts', '-sh', default='127.0.0.1', cls=MutuallyExclusive, mutually_exclusive='source_ring_file', help='Initial contact points for source cluster')
 @click.option('--source-port', '-sp', default=9042, help='Source cluster native transport port')
 @click.option('--source-username', '-su', help='Username to authenticate source cluster', cls=RequiredIf, required_if='source_password')
 @click.option('--source-password', '-spw', help='Password to authenticate source cluster', cls=RequiredIf, required_if='source_username')
@@ -40,7 +68,7 @@ class RequiredIf(click.Option):
 @click.option('--source-client-public-cert', '-spc', help='Path to PEM-format public certificate for 2-way SSL', cls=RequiredIf, required_if='source_client_private_key,source_server_public_cert')
 @click.option('--source-server-public-cert', '-sc', help='Path to PEM-format public certificate of source cluster')
 # Target cluster connection options
-@click.option('--target-hosts', '-th', default='127.0.0.1', required=True, help='Initial contact points for the target cluster')
+@click.option('--target-hosts', '-th', default='127.0.0.1', cls=MutuallyExclusive, mutually_exclusive='target_ring_file', help='Initial contact points for the target cluster')
 @click.option('--target-port', '-tp', default=9042, help='Target cluster native transport port')
 @click.option('--target-username', '-tu', help='Username to authenticate target cluster', cls=RequiredIf, required_if='target_password')
 @click.option('--target-password', '-tpw', help='Password to authenticate target cluster', cls=RequiredIf, required_if='target_username')
@@ -50,32 +78,39 @@ class RequiredIf(click.Option):
 # Output options
 @click.option('--verbose', '-v', is_flag=True, help='Print the cluster topology maps for each cluster')
 def validate_identical_topology(
+  source_ring_file, target_ring_file,
   source_hosts, source_port, source_username, source_password,
   source_client_private_key, source_client_public_cert, source_server_public_cert,
   target_hosts, target_port, target_username, target_password,
   target_client_private_key, target_client_public_cert, target_server_public_cert,
   verbose
 ):
-    source_session = cassandra_utils.CqlSessionProvider(
-      ip_addresses=source_hosts.split(','),
-      port=source_port,
-      username=source_username,
-      password=source_password,
-      client_private_key=source_client_private_key,
-      client_public_cert=source_client_public_cert,
-      server_cert=source_server_public_cert
-    ).new_session()
-    target_session = cassandra_utils.CqlSessionProvider(
-      ip_addresses=target_hosts.split(','),
-      port=target_port,
-      username=target_username,
-      password=target_password,
-      client_private_key=target_client_private_key,
-      client_public_cert=target_client_public_cert,
-      server_cert=target_server_public_cert
-    ).new_session()
-    source_topology = source_session.topology_hashes()
-    target_topology = target_session.topology_hashes()
+    if source_ring_file:
+        source_topology = cassandra_utils.NodetoolRingObject(source_ring_file).parse_topology()
+    else:
+        source_session = cassandra_utils.CqlSessionProvider(
+          ip_addresses=source_hosts.split(','),
+          port=source_port,
+          username=source_username,
+          password=source_password,
+          client_private_key=source_client_private_key,
+          client_public_cert=source_client_public_cert,
+          server_cert=source_server_public_cert
+        ).new_session()
+        source_topology = source_session.topology_hashes()
+    if target_ring_file:
+        target_topology = cassandra_utils.NodetoolRingObject(target_ring_file).parse_topology()
+    else:
+        target_session = cassandra_utils.CqlSessionProvider(
+          ip_addresses=target_hosts.split(','),
+          port=target_port,
+          username=target_username,
+          password=target_password,
+          client_private_key=target_client_private_key,
+          client_public_cert=target_client_public_cert,
+          server_cert=target_server_public_cert
+        ).new_session()
+        target_topology = target_session.topology_hashes()
 
     if source_topology['hashes']['cluster'] == target_topology['hashes']['cluster']:
         click.echo('These two cluster topologies are the same\n')
@@ -89,6 +124,9 @@ def validate_identical_topology(
     else:
         click.echo('No datacenters have a corresponding topology match')
     if verbose:
-        click.echo(f'\n\nSource Topology:\n{source_topology}\n\nTarget Topology:\n{target_topology}')
+        click.echo('\n\nSource Topology:\n{source_topology}\n\nTarget Topology:\n{target_topology}'.format(
+          source_topology=json.dumps(source_topology, indent=2),
+          target_topology=json.dumps(target_topology, indent=2)
+        ))
     else:
         click.echo('\n\nNote: Run with -v to print the full topologies')
